a4f33a2054132667191e5452028671dacbf36778
mohitApisero <mohitmallick17@gmail.com>
2023-08-29 02:56:20 +0530

    remove server dependency

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { makeStyles } from "@material-ui/core";
 import Homepage from "./Pages/HomePage";
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import CoinPage from "./Pages/CoinPage";
 import Header from "./components/Header";
 
@@ -17,13 +17,13 @@ function App() {
   const classes = useStyles();
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className={classes.App}>
         <Header />
         <Route path="/" component={Homepage} exact />
         <Route path="/coins/:id" component={CoinPage} exact />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
